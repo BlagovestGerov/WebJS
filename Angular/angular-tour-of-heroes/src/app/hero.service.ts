42faa71ab3ae8e2fe44a7012a4afce59adea461d
BlagovestGerov <blaje@abv.bg>
2017-11-27 00:26:28 +0200

    Create HeroService

---
+++ Angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  
+  
+  constructor(private messageService: MessageService) { }
+  // Todo: send the message _after_fetching the horoes
+  getHeroes(): Observable<Hero[]>{
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+}
+
+
+
