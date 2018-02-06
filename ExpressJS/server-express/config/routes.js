175034b03352aded4ac279de3f26d7bd334b4752
BlagovestGerov <blaje@abv.bg>
2018-02-06 10:58:35 +0200

    Add login view and logic

--- ExpressJS/server-express/config/routes.js
+++ ExpressJS/server-express/config/routes.js
@@ -9,7 +9,9 @@ module.exports = app =>{
     app.post('/register', controllers.user.registerPost)   
     
     app.post('/logout', controllers.user.logout)
-
+    app.get('/login', controllers.user.loginGet)
+    app.post('/login', controllers.user.loginPost)
+    
     app.all('*', (req,res)=>{
         res.status(404)
         res.send('404 NOT Found')
